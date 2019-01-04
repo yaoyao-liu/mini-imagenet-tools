a2170d7296a78f2676190e02aceff67c2118fa4c
Y2L <yaoyaoliu@live.com>
2019-01-04 15:28:54 +0800

    first commit

---
+++ mini_imagenet_dataloader.py
@@ -0,0 +1,214 @@
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Created by: Yaoyao Liu
+## NUS School of Computing
+## Email: yaoyao.liu@nus.edu.sh
+## Copyright (c) 2018
+##
+## This source code is licensed under the MIT-style license found in the
+## LICENSE file in the root directory of this source tree
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import numpy as np
+import os
+import random
+import scipy.misc as scm
+from tqdm import trange
+
+class MiniImageNetDataLoader(object):
+    def __init__(self, shot_num, way_num, episode_test_sample_num):
+        self.shot_num = shot_num
+        self.way_num = way_num
+        self.episode_test_sample_num = episode_test_sample_num
+        self.num_samples_per_class = episode_test_sample_num + shot_num
+        metatrain_folder = './processed_images/train'
+        metaval_folder = './processed_images/val'
+        metatest_folder = './processed_images/test'
+
+        npy_dir = './episode_filename_list/'
+        if not os.path.exists(npy_dir):
+            os.mkdir(npy_dir)
+
+        self.npy_base_dir = npy_dir + str(self.shot_num) + 'shot_' + str(self.way_num) + 'way/'
+        if not os.path.exists(self.npy_base_dir):
+            os.mkdir(self.npy_base_dir)
+
+        self.metatrain_folders = [os.path.join(metatrain_folder, label) \
+            for label in os.listdir(metatrain_folder) \
+            if os.path.isdir(os.path.join(metatrain_folder, label)) \
+            ]
+        self.metaval_folders = [os.path.join(metaval_folder, label) \
+            for label in os.listdir(metaval_folder) \
+            if os.path.isdir(os.path.join(metaval_folder, label)) \
+            ]
+        self.metatest_folders = [os.path.join(metatest_folder, label) \
+            for label in os.listdir(metatest_folder) \
+            if os.path.isdir(os.path.join(metatest_folder, label)) \
+            ]
+
+    def get_images(self, paths, labels, nb_samples=None, shuffle=True):
+        if nb_samples is not None:
+            sampler = lambda x: random.sample(x, nb_samples)
+        else:
+            sampler = lambda x: x
+        images = [(i, os.path.join(path, image)) \
+            for i, path in zip(labels, paths) \
+            for image in sampler(os.listdir(path))]
+        if shuffle:
+            random.shuffle(images)
+        return images
+
+    def generate_data_list(self, phase='train', episode_num=None):
+        if phase=='train':
+            folders = self.metatrain_folders
+            if episode_num is None:
+                episode_num = 20000
+            if not os.path.exists(self.npy_base_dir+'/train_filenames.npy'):
+                print('Generating train filenames')
+                all_filenames = []
+                for _ in trange(episode_num):
+                    sampled_character_folders = random.sample(folders, self.way_num)
+                    random.shuffle(sampled_character_folders)
+                    labels_and_images = self.get_images(sampled_character_folders, range(self.way_num), nb_samples=self.num_samples_per_class, shuffle=False)
+                    labels = [li[0] for li in labels_and_images]
+                    filenames = [li[1] for li in labels_and_images]
+                    all_filenames.extend(filenames)
+                np.save(self.npy_base_dir+'/train_labels.npy', labels)
+                np.save(self.npy_base_dir+'/train_filenames.npy', all_filenames)
+                print('Train filename and label lists are saved')
+
+        elif phase=='val':
+            folders = self.metaval_folders
+            if episode_num is None:
+                episode_num = 600
+            if not os.path.exists(self.npy_base_dir+'/val_filenames.npy'):
+                print('Generating val filenames')
+                all_filenames = []
+                for _ in trange(episode_num):
+                    sampled_character_folders = random.sample(folders, self.way_num)
+                    random.shuffle(sampled_character_folders)
+                    labels_and_images = self.get_images(sampled_character_folders, range(self.way_num), nb_samples=self.num_samples_per_class, shuffle=False)
+                    labels = [li[0] for li in labels_and_images]
+                    filenames = [li[1] for li in labels_and_images]
+                    all_filenames.extend(filenames)
+                np.save(self.npy_base_dir+'/val_labels.npy', labels)
+                np.save(self.npy_base_dir+'/val_filenames.npy', all_filenames)
+                print('Val filename and label lists are saved')
+                
+        elif phase=='test':
+            folders = self.metatest_folders
+            if episode_num is None:
+                episode_num = 600
+            if not os.path.exists(self.npy_base_dir+'/test_filenames.npy'):
+                print('Generating test filenames')
+                all_filenames = []
+                for _ in trange(episode_num):
+                    sampled_character_folders = random.sample(folders, self.way_num)
+                    random.shuffle(sampled_character_folders)
+                    labels_and_images = self.get_images(sampled_character_folders, range(self.way_num), nb_samples=self.num_samples_per_class, shuffle=False)
+                    labels = [li[0] for li in labels_and_images]
+                    filenames = [li[1] for li in labels_and_images]
+                    all_filenames.extend(filenames)
+                np.save(self.npy_base_dir+'/test_labels.npy', labels)
+                np.save(self.npy_base_dir+'/test_filenames.npy', all_filenames)
+                print('Test filename and label lists are saved')
+        else:
+            print('Please select vaild phase')
+
+    def load_list(self, phase='train'):
+        if phase=='train':
+            self.train_filenames = np.load(self.npy_base_dir + 'train_filenames.npy').tolist()
+            self.train_labels = np.load(self.npy_base_dir + 'train_labels.npy').tolist()
+
+        elif phase=='val':
+            self.val_filenames = np.load(self.npy_base_dir + 'val_filenames.npy').tolist()
+            self.val_labels = np.load(self.npy_base_dir + 'val_labels.npy').tolist()
+
+        elif phase=='test':
+            self.test_filenames = np.load(self.npy_base_dir + 'test_filenames.npy').tolist()
+            self.test_labels = np.load(self.npy_base_dir + 'test_labels.npy').tolist()
+
+        elif phase=='all':
+            self.train_filenames = np.load(self.npy_base_dir + 'train_filenames.npy').tolist()
+            self.train_labels = np.load(self.npy_base_dir + 'train_labels.npy').tolist()
+
+            self.val_filenames = np.load(self.npy_base_dir + 'val_filenames.npy').tolist()
+            self.val_labels = np.load(self.npy_base_dir + 'val_labels.npy').tolist()
+
+            self.test_filenames = np.load(self.npy_base_dir + 'test_filenames.npy').tolist()
+            self.test_labels = np.load(self.npy_base_dir + 'test_labels.npy').tolist()
+
+        else:
+            print('Please select vaild phase')
+
+    def process_batch(self, input_filename_list, input_label_list, batch_sample_num, reshape_with_one=True):
+        new_path_list = []
+        new_label_list = []
+        for k in range(batch_sample_num):
+            class_idxs = range(0, self.way_num)
+            random.shuffle(class_idxs)
+            for class_idx in class_idxs:
+                true_idx = class_idx*batch_sample_num + k
+                new_path_list.append(input_filename_list[true_idx])
+                new_label_list.append(input_label_list[true_idx])
+
+        img_list = []
+        for filepath in new_path_list:
+            this_img = scm.imread(filepath)
+            this_img = this_img / 255.0
+            img_list.append(this_img)
+
+        if reshape_with_one:
+            img_array = np.array(img_list)
+            label_array = one_hot(np.array(new_label_list)).reshape([1, self.way_num*batch_sample_num, -1])
+        else:
+            img_array = np.array(img_list)
+            label_array = self.one_hot(np.array(new_label_list)).reshape([self.way_num*batch_sample_num, -1])
+        return img_array, label_array
+
+    def one_hot(self, inp):
+        n_class = inp.max() + 1
+        n_sample = inp.shape[0]
+        out = np.zeros((n_sample, n_class))
+        for idx in range(n_sample):
+            out[idx, inp[idx]] = 1
+        return out
+
+
+
+    def get_batch(self, phase='train', idx=0):
+        if phase=='train':
+            all_filenames = self.train_filenames
+            labels = self.train_labels 
+        elif phase=='val':
+            all_filenames = self.val_filenames
+            labels = self.val_labels 
+        elif phase=='test':
+            all_filenames = self.test_filenames
+            labels = self.test_labels
+        else:
+            print('Please select vaild phase')
+
+        one_episode_sample_num = self.num_samples_per_class*self.shot_num
+        this_task_filenames = all_filenames[idx*one_episode_sample_num:(idx+1)*one_episode_sample_num]
+        epitr_sample_num = self.shot_num
+        epite_sample_num = self.episode_test_sample_num
+
+        this_task_tr_filenames = []
+        this_task_tr_labels = []
+        this_task_te_filenames = []
+        this_task_te_labels = []
+        for class_k in range(self.way_num):
+            this_class_filenames = this_task_filenames[class_k*self.num_samples_per_class:(class_k+1)*self.num_samples_per_class]
+            this_class_label = labels[class_k*self.num_samples_per_class:(class_k+1)*self.num_samples_per_class]
+            this_task_tr_filenames += this_class_filenames[0:epitr_sample_num]
+            this_task_tr_labels += this_class_label[0:epitr_sample_num]
+            this_task_te_filenames += this_class_filenames[epitr_sample_num:]
+            this_task_te_labels += this_class_label[epitr_sample_num:]
+
+        this_inputa, this_labela = self.process_batch(this_task_tr_filenames, this_task_tr_labels, epitr_sample_num, reshape_with_one=False)
+        this_inputb, this_labelb = self.process_batch(this_task_te_filenames, this_task_te_labels, epite_sample_num, reshape_with_one=False)
+
+        return this_inputa, this_labela, this_inputb, this_labelb
+
+
+
---
+++ mini_imagenet_generator.py
@@ -0,0 +1,107 @@
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## Created by: Yaoyao Liu
+## NUS School of Computing
+## Email: yaoyao.liu@nus.edu.sh
+## Copyright (c) 2018
+##
+## This source code is licensed under the MIT-style license found in the
+## LICENSE file in the root directory of this source tree
+##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import argparse
+import os
+import numpy as np
+import csv
+import glob
+import cv2
+from shutil import copyfile
+from tqdm import tqdm
+
+# argument parser
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--tar_dir',  type=str)
+parser.add_argument('--imagenet_dir',  type=str)
+parser.add_argument('--image_resize',  type=int,  default=84)
+
+args = parser.parse_args()
+
+class MiniImageNetGenerator(object):
+    def __init__(self, input_args):
+        self.input_args = input_args
+        if self.input_args.tar_dir is not None:
+            print('Untarring ILSVRC2012 package')
+            self.imagenet_dir = './imagenet'
+            if not os.path.exists(self.imagenet_dir):
+                os.mkdir(self.imagenet_dir)
+            os.system('tar xvf ' + str(self.input_args.tar_dir) + ' -C ' + self.imagenet_dir)
+        elif self.input_args.imagenet_dir is not None:
+            self.imagenet_dir = self.input_args.imagenet_dir
+        else:
+            print('You need to specify the ILSVRC2012 source file path')
+        self.mini_dir = './mini_imagenet'
+        if not os.path.exists(self.mini_dir):
+            os.mkdir(self.mini_dir)
+        self.image_resize = self.input_args.image_resize
+        
+    def untar_mini(self):
+        self.mini_keys = ['n02110341', 'n01930112', 'n04509417', 'n04067472', 'n04515003', 'n02120079', 'n03924679', 'n02687172', 'n03075370', 'n07747607', 'n09246464', 'n02457408', 'n04418357', 'n03535780', 'n04435653', 'n03207743', 'n04251144', 'n03062245', 'n02174001', 'n07613480', 'n03998194', 'n02074367', 'n04146614', 'n04243546', 'n03854065', 'n03838899', 'n02871525', 'n03544143', 'n02108089', 'n13133613', 'n03676483', 'n03337140', 'n03272010', 'n01770081', 'n09256479', 'n02091244', 'n02116738', 'n04275548', 'n03773504', 'n02606052', 'n03146219', 'n04149813', 'n07697537', 'n02823428', 'n02089867', 'n03017168', 'n01704323', 'n01532829', 'n03047690', 'n03775546', 'n01843383', 'n02971356', 'n13054560', 'n02108551', 'n02101006', 'n03417042', 'n04612504', 'n01558993', 'n04522168', 'n02795169', 'n06794110', 'n01855672', 'n04258138', 'n02110063', 'n07584110', 'n02091831', 'n03584254', 'n03888605', 'n02113712', 'n03980874', 'n02219486', 'n02138441', 'n02165456', 'n02108915', 'n03770439', 'n01981276', 'n03220513', 'n02099601', 'n02747177', 'n01749939', 'n03476684', 'n02105505', 'n02950826', 'n04389033', 'n03347037', 'n02966193', 'n03127925', 'n03400231', 'n04296562', 'n03527444', 'n04443257', 'n02443484', 'n02114548', 'n04604644', 'n01910747', 'n04596742', 'n02111277', 'n03908618', 'n02129165', 'n02981792']
+        
+        for idx, keys in enumerate(self.mini_keys):
+            print('Untarring ' + keys)
+            os.system('tar xvf ' + self.imagenet_dir + '/' + keys + '.tar -C ' + self.mini_dir)
+        print('All the tar files are untarred')
+
+    def process_original_files(self):
+        self.processed_img_dir = './processed_images'
+        split_lists = ['train', 'val', 'test']
+        csv_files = ['./csv_files/train.csv','./csv_files/val.csv', './csv_files/test.csv']
+
+        if not os.path.exists(self.processed_img_dir):
+            os.makedirs(self.processed_img_dir)
+
+        for this_split in split_lists:
+            filename = './csv_files/' + this_split + '.csv'
+            this_split_dir = self.processed_img_dir + '/' + this_split
+            if not os.path.exists(this_split_dir):
+                os.makedirs(this_split_dir)
+            with open(filename) as csvfile:
+                csv_reader = csv.reader(csvfile, delimiter=',')
+                next(csv_reader, None)
+                images = {}
+                print('Reading IDs....')
+
+                for row in tqdm(csv_reader):
+                    if row[1] in images.keys():
+                        images[row[1]].append(row[0])
+                    else:
+                        images[row[1]] = [row[0]]
+
+                print('Writing photos....')
+                for cls in tqdm(images.keys()):
+                    this_cls_dir = this_split_dir + '/' + cls        
+                    if not os.path.exists(this_cls_dir):
+                        os.makedirs(this_cls_dir)
+
+                    lst_files = []
+                    for file in glob.glob(self.mini_dir + "/*"+cls+"*"):
+                        lst_files.append(file)
+
+                    lst_index = [int(i[i.rfind('_')+1:i.rfind('.')]) for i in lst_files]
+                    index_sorted = sorted(range(len(lst_index)), key=lst_index.__getitem__)
+
+                    index_selected = [int(i[i.index('.') - 4:i.index('.')]) for i in images[cls]]
+                    selected_images = np.array(index_sorted)[np.array(index_selected) - 1]
+                    for i in np.arange(len(selected_images)):
+                        if self.image_resize==0:
+                            copyfile(lst_files[selected_images[i]],os.path.join(this_cls_dir, images[cls][i]))
+                        else:
+                            im = cv2.imread(lst_files[selected_images[i]])
+                            im_resized = cv2.resize(im, (self.image_resize, self.image_resize), interpolation=cv2.INTER_AREA)
+                            cv2.imwrite(os.path.join(this_cls_dir, images[cls][i]),im_resized)
+
+
+if __name__ == "__main__":
+    dataset_generator = MiniImageNetGenerator(args)
+    #dataset_generator.untar_mini()
+    dataset_generator.process_original_files()
+
