aae9e2cd40a990f9ab4adc73516b59b6f3038575
Yaoyao Liu <lyy@outlook.sg>
2019-01-04 16:38:32 +0800

    Update mini_imagenet_generator.py

--- mini_imagenet_generator.py
+++ mini_imagenet_generator.py
@@ -2,7 +2,7 @@
 ## Created by: Yaoyao Liu
 ## NUS School of Computing
 ## Email: yaoyao.liu@nus.edu.sh
-## Copyright (c) 2018
+## Copyright (c) 2019
 ##
 ## This source code is licensed under the MIT-style license found in the
 ## LICENSE file in the root directory of this source tree
